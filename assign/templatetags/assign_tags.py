66603ec6f21502a71fc80398363b74d1a0d6ad24
Lodygin Roman Fedorovich <rflodygin_1@edu.hse.ru>
2024-04-17 05:39:48 +0300

    Commit 4

--- assign/templatetags/assign_tags.py
+++ assign/templatetags/assign_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from ..models import Homework
+from ..models import Homework, Course
 from django.utils.safestring import mark_safe
 import markdown
 
@@ -11,9 +11,14 @@ register = template.Library()
 def total_homeworks():
     return Homework.published.count()
 
-@register.inclusion_tag('assign/homework/latest_homeworks.html')
-def show_latest_homeworks(count=5):
-    latest_homeworks = Homework.published.order_by('-publish')[:count]
+@register.inclusion_tag('assign/homework/latest_homeworks.html', takes_context=True)
+def show_latest_homeworks(context, count=5):
+    request = context['request']
+    if request.user.is_authenticated:
+        enrolled_courses = Course.objects.filter(enrollments__student=request.user)
+        latest_homeworks = Homework.published.filter(course__in=enrolled_courses).order_by('-publish')[:count]
+    else:
+        latest_homeworks = Homework.published.none()  # Не возвращает домашние задания для анонимных пользователей
     return {'latest_homeworks': latest_homeworks}
 
 @register.filter(name='markdown')
